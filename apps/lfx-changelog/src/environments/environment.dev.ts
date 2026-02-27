a6c0adfc265c32f4a982280448831cdbdb356cc6
Asitha de Silva <asithade@gmail.com>
2026-02-27 09:20:53 -0800

    fix: remove hardcoded values from chart values
    Signed-off-by: Asitha de Silva <asithade@gmail.com>

--- apps/lfx-changelog/src/environments/environment.dev.ts
+++ apps/lfx-changelog/src/environments/environment.dev.ts
@@ -3,10 +3,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000',
-  auth0: {
-    clientId: '',
-    issuerBaseUrl: '',
-    audience: '',
-  },
+  apiUrl: 'https://changelog.dev.lfx.dev',
 };
